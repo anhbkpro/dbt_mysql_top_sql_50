06d8c31b246b4030fc910b947a199160f31763f3
Anh Lai <laingocanh1990@gmail.com>
2024-05-05 14:24:31 +0700

    add schema for 595. Big Countries

---
+++ schemas/select/world__big_countries.sql
@@ -0,0 +1,7 @@
+Create table If Not Exists world__big_countries (name varchar(255), continent varchar(255), area int, population int, gdp bigint)
+Truncate table world__big_countries
+insert into world__big_countries (name, continent, area, population, gdp) values ('Afghanistan', 'Asia', '652230', '25500100', '20343000000')
+insert into world__big_countries (name, continent, area, population, gdp) values ('Albania', 'Europe', '28748', '2831741', '12960000000')
+insert into world__big_countries (name, continent, area, population, gdp) values ('Algeria', 'Africa', '2381741', '37100000', '188681000000')
+insert into world__big_countries (name, continent, area, population, gdp) values ('Andorra', 'Europe', '468', '78115', '3712000000')
+insert into world__big_countries (name, continent, area, population, gdp) values ('Angola', 'Africa', '1246700', '20609294', '100990000000')
